5c37fd4fd4948a8e8c4ad82dcb90c36acb249744
Thienduy0195 <thienduy0195@gmail.com>
2022-08-11 16:08:13 +0700

    DuyNT push customer interface and component customer information

--- c0222g1-fe/src/app/customer/customer-routing.module.ts
+++ c0222g1-fe/src/app/customer/customer-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CustomerInformationComponent} from "./component/customer-information/customer-information.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'info',
+    component: CustomerInformationComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
