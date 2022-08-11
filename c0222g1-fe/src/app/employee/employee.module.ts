8247ed97e02f7a70eab3f6d0b752f6179557d13f
nguyenduytrungg03 <95289035+nguyenduytrungg03@users.noreply.github.com>
2022-08-11 15:32:17 +0800

    duy trung up compune employee

--- c0222g1-fe/src/app/employee/employee.module.ts
+++ c0222g1-fe/src/app/employee/employee.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { EmployeeRoutingModule } from './employee-routing.module';
+import { EmployeeCreateComponent } from './component/employee-create/employee-create.component';
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    EmployeeRoutingModule
-  ]
+    declarations: [EmployeeCreateComponent],
+    exports: [
+        EmployeeCreateComponent
+    ],
+    imports: [
+        CommonModule,
+        EmployeeRoutingModule
+    ]
 })
 export class EmployeeModule { }
