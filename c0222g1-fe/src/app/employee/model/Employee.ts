d4cebcb8ba45d46f93af729709599a1861929589
nguyenduytrungg03 <95289035+nguyenduytrungg03@users.noreply.github.com>
2022-08-12 17:25:06 +0800

    up create employee

--- c0222g1-fe/src/app/employee/model/Employee.ts
+++ c0222g1-fe/src/app/employee/model/Employee.ts
@@ -1,3 +1,13 @@
 export interface Employee {
-  
-}
+  id: number,
+  code: string,
+  name: string,
+  email: string,
+  phone: string,
+  dob: string,
+  salary: number,
+  startWork: string,
+  statusDelete: number,
+  image: string,
+
+  }
