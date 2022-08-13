40cd71417f75f06495e14b6a323124feb4f6859b
hoang1341995 <hoang1341995@gmail.com>
2022-08-13 12:28:35 +0700

    HoangHN update home-page-customer

--- c0222g1-fe/src/app/customer/customer-routing.module.ts
+++ c0222g1-fe/src/app/customer/customer-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomePageCustomerComponent} from './component/home-page-customer/home-page-customer.component';
+import {AuthGuardCustomerService} from '../authentication/service/auth-guard-customer.service';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'home-page-customer',
+   component: HomePageCustomerComponent,
+    canActivate: [AuthGuardCustomerService]}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
