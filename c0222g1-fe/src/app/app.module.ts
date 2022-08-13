daa1067a7dc8acb62ca7088c1657d564f21cc0d0
Kien1204ngkien1204@gmail.com.com <nguyendtrungkien1204@gmail.com.com>
2022-08-13 19:16:19 +0700

    KienNDT update

--- c0222g1-fe/src/app/app.module.ts
+++ c0222g1-fe/src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './common-component/header/header.component';
-import { FooterComponent } from './common-component/footer/footer.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './common-component/header/header.component';
+import {FooterComponent} from './common-component/footer/footer.component';
 import {AppRoutingModule} from './app-routing.module';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {RouterModule} from '@angular/router';
@@ -11,21 +11,21 @@ import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {ToastrModule} from 'ngx-toastr';
 import {NgxPaginationModule} from 'ngx-pagination';
-import { NotAuthorizedComponent } from './common-component/not-authorized/not-authorized.component';
-import { AngularFireModule } from '@angular/fire';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { environment } from '../environments/environment';
-import {PaymentModule} from "./payment/payment.module";
-import {AuthenticationModule} from "./authentication/authentication.module";
-import {CustomerModule} from "./customer/customer.module";
-import {EmployeeModule} from "./employee/employee.module";
-import {GameModule} from "./game/game.module";
-import {NewsModule} from "./news/news.module";
-import {ComputerModule} from "./computer/computer.module";
-import {ProductModule} from "./product/product.module";
-import {RegistrationModule} from "./registration/registration.module";
-import {HomePageModule} from "./home-page/home-page.module";
-import {StatisticModule} from "./statistic/statistic.module";
+import {NotAuthorizedComponent} from './common-component/not-authorized/not-authorized.component';
+import {AngularFireModule} from '@angular/fire';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
+import {environment} from '../environments/environment';
+import {PaymentModule} from './payment/payment.module';
+import {AuthenticationModule} from './authentication/authentication.module';
+import {CustomerModule} from './customer/customer.module';
+import {EmployeeModule} from './employee/employee.module';
+import {GameModule} from './game/game.module';
+import {NewsModule} from './news/news.module';
+import {ComputerModule} from './computer/computer.module';
+import {ProductModule} from './product/product.module';
+import {RegistrationModule} from './registration/registration.module';
+import {HomePageModule} from './home-page/home-page.module';
+import {StatisticModule} from './statistic/statistic.module';
 
 @NgModule({
   declarations: [
@@ -49,7 +49,7 @@ import {StatisticModule} from "./statistic/statistic.module";
       timeOut: 2000,
       closeButton: true,
       progressBar: true,
-      positionClass: 	'toast-top-right',
+      positionClass: 'toast-top-right',
     }),
     PaymentModule,
     AuthenticationModule,
@@ -66,4 +66,5 @@ import {StatisticModule} from "./statistic/statistic.module";
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
