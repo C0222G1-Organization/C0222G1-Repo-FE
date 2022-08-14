114497e10a61099f4f7af6c7b09ee5870a83f57b
Thienduy0195 <thienduy0195@gmail.com>
2022-08-14 22:00:16 +0700

    DuyNT update payment for front end

--- c0222g1-fe/src/app/payment/component/payment-detail/payment-detail.component.ts
+++ c0222g1-fe/src/app/payment/component/payment-detail/payment-detail.component.ts
@@ -19,29 +19,13 @@ export class PaymentDetailComponent implements OnInit, OnChanges {
   orderProductList = Array<Product>();
   totalPayment: number;
   checkExist = false;
+  quantity = 0;
   count = 1;
   idDelete: number;
 
   imageSrc = 'https://nairobigames.center/wp-content/uploads/2019/10/Nairobi-GDC-FINAL-LOGO-TRANS.png';
 
   productCategoryList: ProductCategory[];
-
-  // productForm: FormGroup = new FormGroup({
-  //   id: new FormControl(0),
-  //   nameProduct: new FormControl(''),
-  //   quantity: new FormControl(0),
-  //   unit: new FormControl(''),
-  //   prices: new FormControl(0),
-  //   imageUrl: new FormControl(''),
-  //   productCategory: new FormGroup({
-  //     id: new FormControl(0, [Validators.required]),
-  //     name: new FormControl('', [Validators.required])
-  //   }),
-  //   deleteStatus: new FormControl(1),
-  //   checked: new FormControl(true)
-  // });
-  quantity = 0;
-
 
   constructor(private productService: ProductService, private toast: ToastrService) {
     this.getProductCategoryList();
@@ -70,34 +54,18 @@ export class PaymentDetailComponent implements OnInit, OnChanges {
 
   updateOrderList() {
     if (this.quantity > 0) {
-      console.log(this.product);
       this.orderProduct = this.product;
-      console.log(this.orderProduct);
       this.orderProduct.quantity = this.quantity;
-      console.log(this.quantity);
-      console.log(this.quantity + 'okeee');
       for (let i = 0; i - 1 < this.orderProductList.length - 1; i++) {
         if (this.orderProductList[i].id === this.orderProduct.id) {
-          console.log(this.count + ' lần trùng');
-          this.count++;
           this.orderProductList[i].quantity += this.quantity;
-          console.log(this.orderProduct.quantity);
-          console.log(this.orderProductList[i].quantity);
-          console.log('đã cộng thêm');
           this.checkExist = true;
         }
       }
-      console.log('check');
-      console.log(this.checkExist);
       if (!this.checkExist) {
         this.orderProductList.push(this.orderProduct);
-        console.log('đã push vào');
       }
       this.checkExist = false;
-      console.log('set');
-      console.log(this.checkExist);
-      console.log(this.orderProductList);
-      console.log('OKE');
     }
   }
 
