407ff73fb6f219332631096303e4adc6c2811485
truongtran2212 <truongtran221298@gmail.com>
2022-08-16 21:22:02 +0700

    TruongTX update product

--- c0222g1-fe/src/app/product/product.module.ts
+++ c0222g1-fe/src/app/product/product.module.ts
@@ -5,10 +5,10 @@ import { ProductRoutingModule } from './product-routing.module';
 import { ProductComponent } from './component/product/product.component';
 import {NgxPaginationModule} from 'ngx-pagination';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { CurrentPipePipe } from './current-pipe.pipe';
+
 
 @NgModule({
-  declarations: [ProductComponent, CurrentPipePipe],
+  declarations: [ProductComponent],
   imports: [
     CommonModule,
     ProductRoutingModule,
