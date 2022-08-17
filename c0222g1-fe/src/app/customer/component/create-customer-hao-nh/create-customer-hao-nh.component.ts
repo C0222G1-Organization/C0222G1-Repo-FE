54c090a71ebbd12ef038b6fade420c3849c93848
C0222G1-NguyenHoangHao <hoanghao9x87@gmail.com>
2022-08-17 08:39:15 +0700

    HaoNH update and merge dev

--- c0222g1-fe/src/app/customer/component/create-customer-hao-nh/create-customer-hao-nh.component.ts
+++ c0222g1-fe/src/app/customer/component/create-customer-hao-nh/create-customer-hao-nh.component.ts
@@ -7,6 +7,7 @@ import {Province} from '../../model/province';
 import {CreateCustomerService} from '../../service/create-customer.service';
 import {Router} from '@angular/router';
 import {ToastrService} from 'ngx-toastr';
+import {Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-create-customer-hao-nh',
@@ -24,40 +25,44 @@ export class CreateCustomerHaoNHComponent implements OnInit {
   provinceForm: FormGroup = new FormGroup({
     province: new FormControl('', Validators.required)
   });
-  customerForm: FormGroup = new FormGroup({
-    id: new FormControl(),
-    // tslint:disable-next-line:max-line-length
-    name: new FormControl('', [Validators.pattern('^[a-zA-Z_ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ\\s?]+$'),
-      Validators.required, this.notBlank, Validators.minLength(5), Validators.maxLength(50)]),
-    dateOfBirth: new FormControl('', [Validators.pattern('^[a-zA-Z\\s?]+$'), this.check16Age]),
-    email: new FormGroup({
-      email: new FormControl('', [Validators.pattern('^[A-Za-z0-9]+@[A-Za-z0-9]+(\\.[A-Za-z0-9]+){1,2}$'), Validators.required])
-    }),
-    phoneNumber: new FormGroup({
-      phone: new FormControl('', [Validators.pattern('^[0-9]{10,12}$'), Validators.required])
-    }),
-    userName: new FormGroup({
-      userName: new FormControl('', [Validators.required, this.notBlank, Validators.pattern('[a-zA-z0-9]{5,50}')])
-    }),
-    password: new FormGroup({
-      // tslint:disable-next-line:max-line-length
-      password: new FormControl('', [Validators.pattern('^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,50}'),
-        Validators.required]),
-      // tslint:disable-next-line:max-line-length
-      confirmPassword: new FormControl('', Validators.required)
-    }, this.checkConfirmPassword),
-    province: new FormControl('', Validators.required),
-    district: new FormControl('', Validators.required),
-    commune: new FormControl('', Validators.required)
-  });
+  customerForm: FormGroup;
 
   constructor(private customerService: CreateCustomerService,
               private router: Router,
-              private toast: ToastrService) {
+              private toast: ToastrService,
+              private titleService: Title) {
+    this.titleService.setTitle('Đăng ký tài khoản');
   }
 
   ngOnInit(): void {
-    this.customerService.getAllProvince().subscribe(value => this.provinceList = value);
+    this.customerService.getAllProvince().subscribe(value => {this.provinceList = value; console.log(value); });
+    this.customerForm =  new FormGroup({
+      id: new FormControl(),
+      // tslint:disable-next-line:max-line-length
+      name: new FormControl('', [Validators.pattern('^[a-zA-Z_ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ\\s?]+$'),
+        Validators.required, this.notBlank, Validators.minLength(5), Validators.maxLength(50)]),
+      dateOfBirth: new FormControl('', [Validators.pattern('^[a-zA-Z\\s?]+$'), this.check16Age]),
+      email: new FormGroup({
+        email: new FormControl('', [Validators.pattern('^[A-Za-z0-9]+@[A-Za-z0-9]+(\\.[A-Za-z0-9]+){1,2}$'), Validators.required])
+      }),
+      phoneNumber: new FormGroup({
+        phone: new FormControl('', [Validators.pattern('^(0|84\\+)[0-9]{9,11}$'),
+          Validators.required])
+      }),
+      userName: new FormGroup({
+        userName: new FormControl('', [Validators.required, this.notBlank, Validators.pattern('[a-zA-z0-9]{5,50}')])
+      }),
+      password: new FormGroup({
+        // tslint:disable-next-line:max-line-length
+        password: new FormControl('', [Validators.pattern('^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,50}'),
+          Validators.required]),
+        // tslint:disable-next-line:max-line-length
+        confirmPassword: new FormControl('', Validators.required)
+      }, this.checkConfirmPassword),
+      province: new FormControl('', Validators.required),
+      district: new FormControl('', Validators.required),
+      commune: new FormControl('', Validators.required)
+    });
   }
 
   private check16Age(abstractControl: AbstractControl): any {
@@ -66,12 +71,21 @@ export class CreateCustomerHaoNHComponent implements OnInit {
     }
     const today = new Date();
     const birthDate = new Date(abstractControl.value);
+    if (birthDate === undefined) {
+      return null;
+    }
     let age = today.getFullYear() - birthDate.getFullYear();
     const m = today.getMonth() - birthDate.getMonth();
     if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
       age--;
     }
-    return (age >= 16) ? null : {not16: true};
+    if (age <= 16) {
+      return {not16: true};
+    } else if (age >= 100) {
+      return {after100: true};
+    } else {
+      return null;
+    }
   }
 
   private notBlank(abstractControl: AbstractControl): any {
@@ -108,10 +122,16 @@ export class CreateCustomerHaoNHComponent implements OnInit {
     // @ts-ignore
     if ($event === '') {
       this.districtList = [];
+      this.customerForm.get('district').setValue('');
       this.communeList = [];
+      this.customerForm.get('commune').setValue('');
     } else {
       this.customerService.getAllDistrict(Number($event)).subscribe(
-        value => this.districtList = value);
+        value => {
+          this.districtList = value;
+          this.customerForm.get('district').setValue('');
+          this.customerForm.get('commune').setValue('');
+        });
     }
   }
 
@@ -119,6 +139,7 @@ export class CreateCustomerHaoNHComponent implements OnInit {
     // @ts-ignore
     if ($event === '') {
       this.communeList = [];
+      this.customerForm.get('commune').setValue('');
     } else {
       this.customerService.getAllCommune(Number($event)).subscribe(
         value => this.communeList = value);
@@ -130,6 +151,7 @@ export class CreateCustomerHaoNHComponent implements OnInit {
     this.updateCustomerDto.password = this.customerForm.value.password.password;
     this.customerService.createCustomer(this.updateCustomerDto).subscribe(
       value => {
+        this.router.navigateByUrl('/sign-in');
         this.toast.success('Đăng ký thành công');
       },
       error => {
@@ -139,7 +161,33 @@ export class CreateCustomerHaoNHComponent implements OnInit {
   }
 
   cancel() {
-    this.customerForm.reset();
+    this.customerForm = new FormGroup({
+      id: new FormControl(),
+      // tslint:disable-next-line:max-line-length
+      name: new FormControl('', [Validators.pattern('^[a-zA-Z_ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ\\s?]+$'),
+        Validators.required, this.notBlank, Validators.minLength(5), Validators.maxLength(50)]),
+      dateOfBirth: new FormControl('', [Validators.pattern('^[a-zA-Z\\s?]+$'), this.check16Age]),
+      email: new FormGroup({
+        email: new FormControl('', [Validators.pattern('^[A-Za-z0-9]+@[A-Za-z0-9]+(\\.[A-Za-z0-9]+){1,2}$'), Validators.required])
+      }),
+      phoneNumber: new FormGroup({
+        // tslint:disable-next-line:max-line-length
+        phone: new FormControl('', [Validators.pattern('^(0|84+)(3[2-9]|5[6|8|9]|7[0|6-9]|8[0-6|8|9]|9[0-4|6-9])[0-9]{7}$'), Validators.required])
+      }),
+      userName: new FormGroup({
+        userName: new FormControl('', [Validators.required, this.notBlank, Validators.pattern('[a-zA-z0-9]{5,50}')])
+      }),
+      password: new FormGroup({
+        // tslint:disable-next-line:max-line-length
+        password: new FormControl('', [Validators.pattern('^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,50}'),
+          Validators.required]),
+        // tslint:disable-next-line:max-line-length
+        confirmPassword: new FormControl('', Validators.required)
+      }, this.checkConfirmPassword),
+      province: new FormControl('', Validators.required),
+      district: new FormControl('', Validators.required),
+      commune: new FormControl('', Validators.required)
+    });
   }
 
   checkUserName($event: Event) {
@@ -147,6 +195,8 @@ export class CreateCustomerHaoNHComponent implements OnInit {
       value => {
         if (value) {
           this.isExitsUser = true;
+        } else {
+          this.isExitsUser = false;
         }
       }
     );
@@ -156,16 +206,19 @@ export class CreateCustomerHaoNHComponent implements OnInit {
   }
 
   checkEmail($event: Event) {
-    if (String($event) === '') {
-      this.isExitsEmail = false;
-    }
     this.customerService.checkEmail(String($event)).subscribe(
       value => {
+        console.log(value);
         if (value) {
           this.isExitsEmail = true;
+        } else {
+          this.isExitsEmail = false;
         }
       }
     );
+    if (String($event) === '') {
+      this.isExitsEmail = false;
+    }
   }
 
   checkPhone($event: Event) {
@@ -173,12 +226,13 @@ export class CreateCustomerHaoNHComponent implements OnInit {
       value => {
         if (value) {
           this.isExitsPhone = true;
+        } else {
+          this.isExitsPhone = false;
         }
       }
     );
     if (String($event) === '') {
       this.isExitsPhone = false;
     }
-    console.log(this.isExitsPhone);
   }
 }
